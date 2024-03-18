1709cb526cc9fb171845b15f33a587010104a92f
KOBOKO23 <kobokomasero@gmail.com>
2024-03-18 08:12:01 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        print("")
+    else:
+        for array in matrix:
+            for n in arr:
+                if n != array[-1]:
+                    print("{:d}".format(n), end=" ")
+                else:
+                    print("{:d}".format(n))
