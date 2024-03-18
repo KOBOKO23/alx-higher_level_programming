6100ea7e3488c3c9133bc5e93c97d03b7d120565
KOBOKO23 <kobokomasero@gmail.com>
2024-03-18 07:56:26 -0700

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,6 @@
+def new_in_list(my_list, idx, element):
+    newList = my_list[0:]
+    if idx >= 0 and idx <= len(newList) - 1:
+        newList[idx] = element
+        return newList
+    return my_list
