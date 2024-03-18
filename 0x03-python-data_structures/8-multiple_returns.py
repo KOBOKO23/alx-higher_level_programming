0a063f54ae1458e112e550c06150f4a676c8b18c
KOBOKO23 <kobokomasero@gmail.com>
2024-03-18 08:26:34 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def max_integer(my_list=[]):
-    sent = None
+    maxi = None
     if len(my_list) > 0:
-        sent = my_list[0]
+        maxi = my_list[0]
         for item in my_list:
-            if item > sent:
-                sent = item
-    return sent
+            if item > maxi:
+                maxi = item
+    return maxi
