485d1146d68e86a08f066e143eb96c2044636e87
KOBOKO23 <kobokomasero@gmail.com>
2024-04-02 06:19:09 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """Defines a class Square"""
 
-class Square:
+iclass Square:
 
     def __init__(self, size=0):
 
         self.__size = size
 
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
