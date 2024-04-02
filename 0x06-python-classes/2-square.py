ac1506cdd5f6ae99e30efb9bda6b530d9016c989
KOBOKO23 <kobokomasero@gmail.com>
2024-04-02 06:21:03 -0700

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,7 +5,7 @@ iclass Square:
 
     def __init__(self, size=0):
 
-        self.__size = size
+        size(int): size of square
 
         if type(size) is not int:
             raise TypeError("size must be an integer")
