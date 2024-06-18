093399ff003a6ce9e6e4c024aee47aba9f86790d
KOBOKO23 <kobokomasero@gmail.com>
2024-06-18 10:11:30 -0700

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,16 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) {
+    if (Number.isInteger(w) && Number.isInteger(h) && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
   print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
+	  if (this.width && this.heaight){
+		  for (let i = 0; i < this.height; i++){
+			  console.log('X'.repeat(this.width));
+		  }
+	  }
   }
 }
